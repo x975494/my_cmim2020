aafe184def9c38c13d6fa3fe0494c1d5bc13b1df
x975494 <60974583+x975494@users.noreply.github.com>
2020-04-13 17:40:56 +0200

    fixed

--- Assignment_D/Mechanism.m
+++ Assignment_D/Mechanism.m
@@ -9,20 +9,18 @@ phi = deg2rad(30);
 u0 = [0; b + a];
 
 F = @(u) constraint(u, a, b, phi);
-J = @(u) jacobian(u, a);
+J = @(u) jacobian(u, b);
 
 eps = 1e-4;
-[u, iteration_counter] = NR_method(F, J, u0, eps);
+u = NR_method(F, J, u0, eps);
 
 fprintf('\n\tMechanism valid position is for d = %gm and phi1 = %gdeg\n\n', ...
     u(2), rad2deg(u(1)));
 
 function P = constraint(u, a, b, phi)
-theta = u(1);
-d = u(2);
 
-P = [a * cos(phi) + b * cos(theta) - d
-    a * sin(phi) - b * sin(theta)];
+P = [a * cos(phi) + b * cos(u(1)) - u(2)
+    a * sin(phi) - b * sin(u(1))];
 end
 
 function P = jacobian(u, b)
--- Assignment_D/Mechanism_vel.m
+++ Assignment_D/Mechanism_vel.m
@@ -2,42 +2,65 @@
 % u = [theta; d];
 a = 0.1;    % m
 b = 0.2;    % m
-w = 1;      % rad/s
-tn = floor(2*pi/w);     % s
+w = -1;      % rad/s
+c = 3;      % cycle move
+tn = abs(floor(2*pi/w*c));     % s
 dt = 0.1;   % s
 eps = 1e-4;
+t = 0 : dt : tn;
+n = length(t);
 % set starting point
 u0 = [0; b + a];
 
+u = zeros(2,n);
 
 hold on
-    for t = 1 : dt: tn
+    for ii = 1 : n
     
-    phi = pi/6+w*t;
+    phi = pi/6+w*t(ii);
     F = @(u) constraint(u, a, b, phi);
-    J = @(u) jacobian(u, a);
+    J = @(u) jacobian(u, b);
 
-    
-    [u, iteration_counter] = NR_method(F, J, u0, eps);
+    up = NR_method(F, J, u0, eps);
 
-    plot(t, rad2deg(u(1)), t, u(2))
+    u0 = up;
+    u(:,ii) = up;
     
-    u0 = u;
     end
-    
+
+subplot(2,1,1)
+hold on
+plot(t,u(1,:),'r-')
+plot(t,u(2,:),'b-')    
 title('Move plot')
 xlabel('t [s]')
+grid on
+legend('$\theta$','$d$','interpreter','latex')
+hold off
+
+v = zeros(2,n);
+phi_v = pi/6+w*t;
+v(1,:) = (a*cos(phi_v)*w)./(0.2*cos(u(1,:)));
+v(2,:) = -a*sin(phi_v)*w-b*sin(u(1,:)).*v(1,:);
+
+subplot(2,1,2)
+hold on
+plot(t,v(1,:),'r-')
+plot(t,v(2,:),'b-')    
+title('Velocity plot')
+xlabel('t [s]')
+grid on
+hold off
+
 
 function P = constraint(u, a, b, phi)
-theta = u(1);
-d = u(2);
 
-P = [a * cos(phi) + b * cos(theta) - d
-    a * sin(phi) - b * sin(theta)];
+P = [a * cos(phi) + b * cos(u(1)) - u(2)
+    a * sin(phi) - b * sin(u(1))];
 end
 
 function P = jacobian(u, b)
-theta = u(1);
-P = [-b * sin(theta), -1
-    -b * cos(theta), 0];
+
+P = [-b * sin(u(1)), -1
+    -b * cos(u(1)), 0];
 end
